b39a8c19e8814848f47f35e85c02d0083a89f69f
Kakeru Nemoto <drumgadaisuki@gmail.com>
2018-10-09 08:10:45 +0900

    Initial Commit

---
+++ ScheduleManager/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ScheduleManager
+//
+//  Created by 根本翔 on 2018/10/09.
+//  Copyright © 2018 根本翔. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
---
+++ ScheduleManagerTests/ScheduleManagerTests.swift
@@ -0,0 +1,34 @@
+//
+//  ScheduleManagerTests.swift
+//  ScheduleManagerTests
+//
+//  Created by 根本翔 on 2018/10/09.
+//  Copyright © 2018 根本翔. All rights reserved.
+//
+
+import XCTest
+@testable import ScheduleManager
+
+class ScheduleManagerTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
